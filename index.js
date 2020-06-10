7f79dacbe5cae01e250a21f39af8e7a6a9d08711
Mohamed Sheikh <mohamed-sheikh1@outlook.com>
2020-06-10 18:43:36 +0100

    latest

--- index.js
+++ index.js
@@ -1,53 +1,59 @@
-const axios = require("axios");
 const OAuth = require("oauth");
 const motivation = require("./tweets.json");
-
 const fs = require("fs");
 
-// if (process.env.USER && process.env.USER === "MohamedS") {
-//   let envResult = (resolve, reject) => {
-//     fs.readFile("../../Documents/config.json", "utf8", (err, data) => {
-//       if (err) {
-//         console.log(err);
-//         reject(err);
-//       }
-//       resolve(data);
-//       process.env = JSON.parse(data);
-//     });
-//   };
-//   return new Promise(envResult);
-// }
-console.log(process.env.ConsumerKey);
-
-const oauth = new OAuth.OAuth(
-  "https://api.twitter.com/oauth/request_token",
-  "https://api.twitter.com/oauth/access_token",
-  process.env.ConsumerKey,
-  process.env.ConsumerSecret,
-  "1.0A",
-  null,
-  "HMAC-SHA1"
-);
-
-let result = (resolve, reject) => {
-  oauth.post(
-    `https://api.twitter.com/1.1/statuses/update.json`,
-    process.env.AccessToken,
-    process.env.TokenSecret,
-    { status: "HELLO from script" },
-    (e, data, response) => {
-      if (response) {
-      }
-      if (data) {
-        console.log("SENT");
-        resolve(data);
-      }
-      if (e) {
-        console.log(e);
-        reject(e);
-      }
+async function exportHandler() {
+  async function initialise() {
+    if (process.env.USER && process.env.USER === "MohamedS") {
+      let result = (resolve, reject) => {
+        fs.readFile("../../Documents/config.json", "utf8", (err, data) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          }
+          process.env = JSON.parse(data);
+          resolve(JSON.parse(data));
+        });
+      };
+      return new Promise(result);
     }
-  );
-};
+  }
+  async function sendTweet() {
+    const oauth = new OAuth.OAuth(
+      "https://api.twitter.com/oauth/request_token",
+      "https://api.twitter.com/oauth/access_token",
+      process.env.ConsumerKey,
+      process.env.ConsumerSecret,
+      "1.0A",
+      null,
+      "HMAC-SHA1"
+    );
 
-return new Promise(result);
+    let result = (resolve, reject) => {
+      oauth.post(
+        `https://api.twitter.com/1.1/statuses/update.json`,
+        process.env.AccessToken,
+        process.env.TokenSecret,
+        { status: "HELLO from script1" },
+        (e, data, response) => {
+          if (response) {
+          }
+          if (data) {
+            console.log("SENT");
+            resolve(data);
+          }
+          if (e) {
+            console.log(e);
+            reject(e);
+          }
+        }
+      );
+    };
+
+    return new Promise(result);
+  }
+  await initialise();
+  sendTweet();
+}
+
+exportHandler();
